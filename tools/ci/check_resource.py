3f7c75277dc9ba5729a9b758e78d665b1c4eb1c9
KhazixW2 <3196497671@qq.com>
2026-02-07 16:35:35 +0800

    ci: 资源检查客户端版本时，如果不是base 优先post base

--- tools/ci/check_resource.py
+++ tools/ci/check_resource.py
@@ -29,8 +29,14 @@ def main():
         sys.exit(1)
 
     Tasker.set_stdout_level(LoggingLevelEnum.All)
-
     dirs = [Path(arg) for arg in sys.argv[1:]]
+    if sys.argv[1] != "base":
+        # 如果不是检查base目录，添加base目录到检查列表
+        base_dir = Path("assets/resource/base")
+        if base_dir not in dirs:
+            dirs.insert(0, base_dir)
+            print(f"Added base directory to check list: {base_dir}")
+
     if not check(dirs):
         sys.exit(1)
 
